8bb6867959a52b0ae0baf5eed3599708074a2d10
Peter Fačko <pfacko1@gmail.com>
2019-08-16 14:08:46 +0200

    smarter accumulate

--- src/accumulate.hpp
+++ src/accumulate.hpp
@@ -1,11 +1,27 @@
 #pragma once
 #include "range.hpp"
 #include "remove_cv.hpp"
+#include "constructible.hpp"
+#include "constructible_template.hpp"
 
-template <range_c R, typename T = remove_cv<range_base<R>>>
-auto accumulate(R r, T init = T())
+template <typename R, typename T = remove_cv<range_base<R>>>
+requires range_type<R>::v || constructible_template<range, R>::v
+auto accumulate(const R& x, T init)
 {
-	for (; r; ++r)
-		init += *r;
-	return init;
+	if constexpr (range_type<R>::v)
+	{
+		auto r = x;
+		for (; r; ++r)
+			init += *r;
+		return init;
+	}
+	else
+		return accumulate(range(x), init);
+}
+
+template <typename R, typename T = remove_cv<range_base<R>>>
+requires constructible<T>::v
+auto accumulate(const R& x)
+{
+	return accumulate(x, T());
 }
