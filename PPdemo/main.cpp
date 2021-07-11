922d269b11674da9860ad7cd3c00093ded8c9fbd
Fačko Peter <150-fackop@users.noreply.localhost>
2021-07-11 12:30:05 +0200

    .

--- PPdemo/main.cpp
+++ PPdemo/main.cpp
@@ -1,41 +1,10 @@
-#include <iomanip>
 #include <iostream>
-#include <variant>
-#include <vector>
 
-#include "PP/array_vector.hpp"
-#include "PP/max_default.hpp"
-#include "PP/simple_vector.hpp"
-#include "PP/size_of.hpp"
-#include "PP/small_optimized_vector.hpp"
-#include "PP/tuple_cartesian_product.hpp"
-#include "PP/tuple_map_to_array.hpp"
-#include "PP/variant.hpp"
-
-void f(auto x, auto y)
-{
-	std::cout << sizeof(x) << ' ' << sizeof(y) << '\n';
-	std::cout << x << ' ' << y << '\n';
-}
+#include "PP/tuple.hpp"
 
 int main()
 {
-	auto table =
-		PP::applier(
-			[](auto t, auto u) -> void (*)(const void*, const void*)
-			{
-				return [](const void* x, const void* y)
-				{
-					f(*reinterpret_cast<const PP_GET_TYPE(t)*>(x),
-					  *reinterpret_cast<const PP_GET_TYPE(u)*>(y));
-				};
-			}) <
-		PP::tuple_cartesian_product_pack(PP::type_tuple<char, short, int>,
-										 PP::type_tuple<float, double>);
-
-	short x = 4;
-	float y = 2.4;
-	table[2](&x, &y);
+	PP::tuple<char, short, int> a;
 
 	std::cout.flush();
 	return 0;
