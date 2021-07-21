4be69ca428a9601369667f170b72fa13f5f7b676
Fačko Peter <150-fackop@users.noreply.localhost>
2021-07-21 17:36:22 +0200

    .

--- PP/variant.hpp
+++ PP/variant.hpp
@@ -17,126 +17,130 @@
 
 namespace PP
 {
-	namespace detail
+namespace detail
+{
+class visit_helper;
+}
+
+template <typename... TypeClasses>
+class variant
+{
+	friend detail::visit_helper;
+
+	std::variant<wrap_ref_t<TypeClasses>...> v;
+
+public:
+	constexpr variant(placeholder_t, auto&& x) noexcept
+		: v(PP_F(x))
+	{}
+
+	constexpr bool holds_alternative(concepts::type auto t) const noexcept
 	{
-		class visit_helper;
+		return std::holds_alternative<wrap_ref_t<PP_GT(t)>>(v);
+	}
+};
+
+namespace detail
+{
+template <typename... T>
+class variant2
+{
+	friend detail::visit_helper;
+
+	static constexpr auto types = type_tuple<T...>;
+	static constexpr auto max =
+		(id_copy | der | max_default |
+	     tuple_map_make_array)(partial_tag, value_1, types);
+
+	static constexpr auto type_eql = functor(
+		[](auto t, auto u)
+		{
+			return value<PP_COPY_TYPE(t) == PP_COPY_TYPE(u)>;
+		});
+
+	size_t index;
+	alignas(max(alignment_of)) char buffer[max(size_of)];
+
+	static constexpr size_t get_type_index(concepts::type auto t) noexcept
+	{
+		return tuple_find(type_eql * t, types);
 	}
 
-	template <typename... TypeClasses>
-	class variant
+public:
+	constexpr variant2(concepts::type auto t, auto&&... args)
+		: index(0)
+		, buffer()
 	{
-		friend detail::visit_helper;
+		auto i = get_type_index(t);
 
-		std::variant<wrap_ref_t<TypeClasses>...> v;
+		static_assert(PP_GET_VALUE(i) != sizeof...(T),
+		              "type is not in this variant");
 
-	public:
-		constexpr variant(placeholder_t, auto&& x) noexcept
-			: v(PP_F(x))
-		{}
+		index = *i;
 
-		constexpr bool holds_alternative(concepts::type auto t) const noexcept
-		{
-			return std::holds_alternative<wrap_ref_t<PP_GT(t)>>(v);
-		}
-	};
+		construct_at_pack(reinterpret__cast(add_pointer <<= types[i], buffer),
+		                  PP_F(args)...);
+	}
 
-	template <typename... T>
-	class variant2
+	constexpr bool holds_alternative(concepts::type auto t) const noexcept
 	{
-		friend detail::visit_helper;
+		return *get_type_index(t) == index;
+	}
 
-		static constexpr auto types = type_tuple<T...>;
-		static constexpr auto max =
-			(id_copy | der | max_default |
-		     tuple_map_make_array)(partial_tag, value_1, types);
+	constexpr ~variant2()
+	{}
 
-		static constexpr auto type_eql = functor(
-			[](auto t, auto u)
-			{
-				return value<PP_COPY_TYPE(t) == PP_COPY_TYPE(u)>;
-			});
-
-		size_t index;
-		alignas(max(alignment_of)) char buffer[max(size_of)];
-
-		static constexpr size_t get_type_index(concepts::type auto t) noexcept
-		{
-			return tuple_find(type_eql * t, types);
-		}
-
-	public:
-		constexpr variant2(concepts::type auto t, auto&&... args)
-			: index(0)
-			, buffer()
-		{
-			auto i = get_type_index(t);
-
-			static_assert(PP_GET_VALUE(i) != sizeof...(T),
-			              "type is not in this variant");
-
-			index = *i;
-
-			construct_at_pack(
-				reinterpret__cast(add_pointer <<= types[i], buffer),
-				PP_F(args)...);
-		}
-
-		constexpr bool holds_alternative(concepts::type auto t) const noexcept
-		{
-			return *get_type_index(t) == index;
-		}
-
-		constexpr ~variant2()
-		{}
-
-		// clang-format off
+	// clang-format off
 		constexpr ~variant2()
 			requires (concepts::trivially_destructible<T> && ...)
 		= default;
-		// clang-format on
-	};
+	// clang-format on
+};
+}
 
-	namespace detail
+namespace detail
+{
+struct visit_helper
+{
+	template <typename V, typename R, typename... T>
+	using functor = R (*)(V, T...);
+
+	static PP_FUNCTOR(visit, auto&& visitor, auto&&... variants)
+		-> decltype(auto)
 	{
-		struct visit_helper
-		{
-			template <typename V, typename R, typename... T>
-			using functor = R (*)(V, T...);
+		return std::visit(compose(PP_REF_WRAP(visitor), unwrap_ref),
+		                  PP_F(variants).v...);
+	});
 
-			static PP_FUNCTOR(visit, auto&& visitor, auto&&... variants)
-				-> decltype(auto)
-			{
-				return std::visit(compose(PP_REF_WRAP(visitor), unwrap_ref),
-				                  PP_F(variants).v...);
-			});
+	static PP_FUNCTOR(visit2, auto&& visitor, auto&&... variants)
+		-> decltype(auto)
+	{
+		auto table_tuple =
+			PP::applier(
+				[](auto... types)
+				{
+					return
+						[](auto&& variant, auto&... buffers) -> decltype(auto)
+					{
+						return PP_F(variant)(
+							reinterpret__cast(PP_COPY_TYPE(types), buffers)...);
+					};
+				}) +
+			tuple_cartesian_product_pack(variants.types...);
 
-			static PP_FUNCTOR(visit2, auto&& visitor, auto&&... variants)
-				-> decltype(auto)
-			{
-				auto table_tuple =
-					PP::applier(
-						[](auto... types)
-						{
-							return [](auto&& variant,
-					                  auto&... buffers) -> decltype(auto)
-					        {
-								return PP_F(variant)(
-									reinterpret__cast(PP_COPY_TYPE(types),
-						                              buffers)...);
-							};
-						}) +
-					tuple_cartesian_product_pack(variants.types...);
+		// tuple_map_make_array(, )
 
-				// tuple_map_make_array(, )
+		// Template<functor>[PP_DECLTYPE(visitor) += type_void +=
+		//					  make_iterate_tuple(PP_SIZEOF___(variants),
+		//										 type<char&>)]
+	});
+};
+}
 
-				// Template<functor>[PP_DECLTYPE(visitor) += type_void +=
-				//					  make_iterate_tuple(PP_SIZEOF___(variants),
-				//										 type<char&>)]
-			});
-		};
-	}
+constexpr inline auto visit = detail::visit_helper::visit;
 
-	constexpr inline auto visit = detail::visit_helper::visit;
-
-	constexpr inline auto visit2 = detail::visit_helper::visit2;
+namespace detail
+{
+constexpr inline auto visit2 = detail::visit_helper::visit2;
+}
 }
