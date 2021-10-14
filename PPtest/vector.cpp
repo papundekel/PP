23a52af0a6b47935a5f3d2281dc1e02ae30ee988
Peter Fačko <pfacko1@gmail.com>
2021-10-14 21:23:36 +0200

    .

--- PPtest/vector.cpp
+++ PPtest/vector.cpp
@@ -8,10 +8,8 @@ void vector()
 {
     PP::vector<int> v;
 
-    v.push_back(0);
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
+    for (int i = 0; i != 17; ++i)
+        v.push_back(i);
 
     for (const auto& e : v)
         std::cout << e << ", ";
